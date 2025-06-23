1da15c2f4255ba458ec9279f5acd7a313f626607
Rajeshan Maharjan <rajeshanmaharjan.25@gmail.com>
2025-06-23 19:28:31 +0545

    modifying the interface by adding the a function and call it

--- basics/Day_02_ts/L3/interface.js
+++ basics/Day_02_ts/L3/interface.js
@@ -1,2 +1,11 @@
-"use strict";
 console.log("Learning interface");
+var user1;
+user1 = {
+    name: "maharjan",
+    age: 21,
+    greet: function (text) {
+        console.log("".concat(text, " ").concat(this.name));
+    }
+};
+console.log(user1);
+user1.greet("hi there, i am Rajeshan");
--- basics/Day_02_ts/L3/interface.ts
+++ basics/Day_02_ts/L3/interface.ts
@@ -1,1 +1,19 @@
 console.log("Learning interface");
+// interface  it is the representation of a object.
+interface UserInfo {
+    name:string;
+    age:number;
+    greet(text:string):void
+}
+let user1: UserInfo;
+user1 = {
+    name:"maharjan",
+    age:21,
+    greet(text):void {
+        console.log(`${text} ${this.name}`);
+        
+    }
+}
+console.log(user1);
+
+user1.greet("hi there, i am Rajeshan")
